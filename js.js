c337dbd7b36bffa705d549973c52496950a97b97
Paxnar <67262211+Paxnar@users.noreply.github.com>
2020-06-22 23:01:29 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+var chasi = prompt("Сколько часов?", ' ');
+var str1 = prompt("Введите своё имя, пожалуйста  ", ' ');
+var sixty = 60;
+sixty *= sixty
+sixty *= chasi 
+alert("Мне кажется вас зовут "+str1+"! А секунд в часах, которые вы ввели - "+sixty);
+ 
