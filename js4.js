8cac8dd866d789fc5a5a92b3116e75fa4306070c
Paxnar <67262211+Paxnar@users.noreply.github.com>
2020-06-25 09:31:22 +0300

    задание на 25.06

---
+++ js4.js
@@ -0,0 +1,53 @@
+/*var a = 2;
+var b = 3;
+alert(a**b);
+
+var counter = 5;
+counter++;
+counter--;
+alert(counter);
+
+var a = confirm("Today Wednesday?");
+
+var a = 6;
+if(a==6){
+	alert("Var a = 6!");
+}
+else if(a==5){
+	alert("var a = 5");
+}else{
+	alert("var a ne = 6 and 5!");
+}*/
+
+/*var age = 16;
+if(age>15 || age==17){
+alert(age);
+}else
+{
+	alert("Mne ne 16!");
+}*/
+var ch1 = 4;
+alert("Число ch1 = " + ch1);
+alert("Если возвести его в пятую степень, мы получим: " + ch1**5);
+
+var seconds = 5;
+if(seconds >= 10){
+	alert(seconds);
+}else{
+	alert(seconds**2);
+	seconds++;
+	seconds--;
+}
+var od=1;
+var dv=2;
+if(od==1 && dv==2){
+	alert("од=1 дв=2");
+}else if(od==1 || dv==2){
+	alert("либо од=1, либо дв=2");
+}else if(!od==1 && !dv==2)
+{
+	alert("од не равно 1, либо дв не равно 2")
+}
+
+
+
